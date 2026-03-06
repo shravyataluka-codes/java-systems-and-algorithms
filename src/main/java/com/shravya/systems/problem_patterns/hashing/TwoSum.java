267773bfa3079d572b8fbb72ae9287851235d43f
shravyataluka-codes <shrvyataluka20@gmail.com>
2026-03-06 22:43:16 +0530

    Add problem discription and Edge cases for Two Sum usng Hashing Technique

--- src/main/java/com/shravya/systems/problem_patterns/hashing/TwoSum.java
+++ src/main/java/com/shravya/systems/problem_patterns/hashing/TwoSum.java
@@ -1,5 +1,28 @@
 package com.shravya.systems.problem_patterns.hashing;
-
+/**
+ * Problem: Two Sum
+ *
+ * Category-Array/HashMap
+ * Pattern used : Hashing technique
+ *
+ * Time complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Approach:
+ * --
+ * --
+ * --
+ * --
+ *
+ *
+ *
+ * Edge Cases:
+ * -- duplicate array
+ * -- Single element
+ * -- empty array
+ * -- negative values
+ *
+ */
 public class TwoSum {
     public void twoSum(int[] arr, int target) {
 
